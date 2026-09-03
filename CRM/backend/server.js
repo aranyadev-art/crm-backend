74d5e46791ed94053ef6263d632d8be43e7c603d
aranyadev-art <2906dev@gmail.com>
2026-09-03 17:41:31 +0530

    Set Socket.IO instance in the app

--- CRM/backend/server.js
+++ CRM/backend/server.js
@@ -61,6 +61,8 @@ const io = new Server(server, {
   },
 });
 
+app.set("io", io);
+
 // ========================================
 // SOCKET.IO AUTHENTICATION
 // ========================================
